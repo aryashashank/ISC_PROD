baa5d46facf7bc1ab25389676171db3ed04f6064
Shashank Arya <sasharya05@gmail.com>
2018-04-24 22:37:45 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+    var elem = document.querySelector('.slider');
+    var options = {
+        indicators: false
+    };
+    var instance = M.Slider.init(elem, options);
+
+
+    var corousel = document.querySelector('.carousel');
+    var corouselInstance = M.Carousel.init(corousel, { fullWidth: false });
+
+
+    autoplay();
+
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 2500);
+    }
+    $('.parallax').parallax();
+
+    $('.sidenav').sidenav();
+
+
+    $(document).scroll(function () {
+        let boundingBox = $("#navbar-sticky").get(0).getBoundingClientRect();
+        if (boundingBox.top == 0) {
+            $('#navbar-sticky').addClass('white');
+            $('#hamberger').addClass('black-text').removeClass('white-text');
+            $('.navbtn').addClass('black-text').removeClass('white-text');
+
+        }
+        else {
+            $('#navbar-sticky').removeClass('white');
+            $('#hamberger').addClass('white-text').removeClass('black-text');
+            $('.navbtn').addClass('white-text').removeClass('black-text');
+        }
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
