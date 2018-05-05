336dea64c33eb20b6b4d1bfb206b6d551c8466c7
Shashank Arya <sasharya05@gmail.com>
2018-05-05 22:39:00 +0530

    Home Page Design Completed

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ $(document).ready(function () {
     };
     var instance = M.Slider.init(elem, options);
 
-
+    function toggleTitle(){
+        console.log("heroo");
+            $(".showcase-title").toggleClass('hide');
+    }
     var corousel = document.querySelector('.carousel');
     var corouselInstance = M.Carousel.init(corousel, { fullWidth: false, numVisible:15,dist:0,indicators:true });
-
+    
 
     autoplay();
 
@@ -21,17 +24,20 @@ $(document).ready(function () {
 
     $('.sidenav').sidenav();
 
-    $('.materialboxed').materialbox();
+    $('.materialboxed').materialbox( {onOpenEnd:toggleTitle, onCloseStart:toggleTitle});
     $(document).scroll(function () {
-        let boundingBox = $("#navbar-sticky").get(0).getBoundingClientRect();
-        if (boundingBox.top == 0) {
+        let boundingBox = $(".homeSlider").get(0).getBoundingClientRect();
+        if (boundingBox.top < 0) {
             $('#navbar-sticky').addClass('white');
+            $('.headingContainer').addClass('white');
             $('#hamberger').addClass('black-text').removeClass('white-text');
             $('.navbtn').addClass('black-text').removeClass('white-text');
 
         }
         else {
+            console.log(boundingBox.top);
             $('#navbar-sticky').removeClass('white');
+            $('.headingContainer').removeClass('white');
             $('#hamberger').addClass('white-text').removeClass('black-text');
             $('.navbtn').addClass('white-text').removeClass('black-text');
         }
